64d00f4103939ade5196f4d73b338daf9cba1f60
AlifApr <87607363+AlifApr@users.noreply.github.com>
2022-04-27 22:22:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>PA Web Parkir</title>
+		<meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="shortcut icon" href="images/parkir.png">
+		<link rel="stylesheet" href="index.css">
+	</head>
+	<body>
+	<!--Form baru-->
+	<div class="login-button">
+		<div class="header">
+			<h1>Sistem Parkir</h1>
+		</div>
+		<div class="body">
+			<div class="body-content">
+				<img src="images/parkir.png" alt="">
+				<p>Silahkan login dengan menekan tombol dibawah</p>
+				<a href="login.php"><button class="btn">Login</button></a>
+			</div>
+		</div>
+	</div>
+
+	</body>
+</html>
